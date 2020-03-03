dd0e46731f0fda694d1f8e3f756442e991a5980a
David Figueroa <democres@hotmail.es>
2020-03-03 18:44:32 -0500

    Remove Unnecessary files

--- Pods/Target Support Files/Pods-socialFeedApp-socialFeedAppUITests/Pods-socialFeedApp-socialFeedAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_socialFeedApp_socialFeedAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_socialFeedApp_socialFeedAppUITests
-@end
--- Pods/Target Support Files/Pods-socialFeedApp/Pods-socialFeedApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_socialFeedApp : NSObject
-@end
-@implementation PodsDummy_Pods_socialFeedApp
-@end
